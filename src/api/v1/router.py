220b16b46f3c5c1dbf6a891aa9f78509c671d556
Michael Frank <mffranks34@gmail.com>
2026-06-08 22:34:33 +0100

    update to deploy conf

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import items, worker, worker_ws
+from api.v1.endpoints import worker_ws
 
 router = APIRouter()
 
-router.include_router(items.router, prefix="/items", tags=["items"])
-router.include_router(worker.router, prefix="/workers", tags=["workers"])
 router.include_router(worker_ws.router, prefix="/workers", tags=["workers"])
